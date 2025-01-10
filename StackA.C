558a2dca869dc5d176a8331a5911ab2a7311a695
Sivapriyadarsini.A <sivapriyadarsiniannamalai@gmail.com>
2025-01-10 23:51:53 +0530

    Update StackA.C

--- StackA.C
+++ StackA.C
@@ -1,93 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100 // Maximum size of the stack
 
-#include<stdio.h>
-#include<conio.h>
-int stack[100],i,j,ch,n,top_ele=-1;
-void push();
-void pop();
-void display();
-void top_ele();
-void main()
-{
-    clrscr();
-    printf("Enter the no of elements:");
-    scanf("%d",&n);
-    printf("\nOperations on stack");
-    while(ch!=5)
-    {
-        printf("\n1.PUSH\n2.POP\n3.DISPLAY\n4.top_ele ELEMENT\n5.EXIT");
-        printf("\nEnter the choice:");
-        scanf("%d",&ch);
-        switch(ch)
-        {
-            case 1:{
+int stack[MAX];
+int top = -1; // Initialize the stack as empty
+
+// Function to push an element into the stack
+void push() {
+    int val;
+    if (top == MAX - 1) { // Check if the stack is full
+        printf("\nStack Overflow! Cannot push more elements.\n");
+    } else {
+        printf("\nEnter the element to push: ");
+        scanf("%d", &val);
+        top++;
+        stack[top] = val;
+        printf("%d pushed into the stack.\n", val);
+    }
+}
+
+// Function to pop an element from the stack
+void pop() {
+    if (top == -1) { // Check if the stack is empty
+        printf("\nStack Underflow! No elements to pop.\n");
+    } else {
+        int popped = stack[top];
+        top--;
+        printf("%d popped from the stack.\n", popped);
+    }
+}
+
+// Function to view the top element of the stack
+void peek() {
+    if (top == -1) { // Check if the stack is empty
+        printf("\nThe stack is empty. No top element.\n");
+    } else {
+        printf("\nThe top element is: %d\n", stack[top]);
+    }
+}
+
+// Function to display all elements of the stack
+void display() {
+    if (top == -1) { // Check if the stack is empty
+        printf("\nThe stack is empty.\n");
+    } else {
+        printf("\nStack elements are:\n");
+        for (int i = 0; i <= top; i++) { // Loop through the stack
+            printf("%d ", stack[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int choice;
+
+    while (1) { // Infinite loop for the menu
+        printf("\n--- Stack Operations Menu ---\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Peek\n");
+        printf("4. Display\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
                 push();
                 break;
-            }
-            case 2:{
+            case 2:
                 pop();
                 break;
-            }
-            case 3:{
+            case 3:
+                peek();
+                break;
+            case 4:
                 display();
                 break;
-            }
-            case 4:{
-                top_ele();
-                break;
-            }
-            case 5:{
-                printf("\nExiting");
-                break;
-            }
-            default:printf("Invalid Choice");
-        }          
-    };
-getch();
-}
+            case 5:
+                printf("\nExiting the program.\n");
+                exit(0);
+            default:
+                printf("\nInvalid choice! Please try again.\n");
+        }
+    }
 
-void push()
-{
-    int val;
-    if(top_ele==n-1)
-        printf("\nOverflow");
-    else
-    {
-        printf("\nEnter the element:");
-        scanf("%d",&val);
-        top_ele++;
-        stack[top_ele]=val;
-    }
- }
-void pop()
- {
-    if(top_ele==-1)
-        printf("\nUnderflow");
-    else
-    {
-        int val;
-        val=stack[top_ele];
-        top_ele--;
-        printf("\nElement deleted");
-    }
-}
-void display()
-{
-    printf("\nThe elements are:");
-    for(i=top_ele;i>=0;i--)
-    {
-        printf("%d\t",stack[i]);
-    }
-    if(top_ele==-1)
-        printf("\nStack is empty");
-}
-void top_ele()
-{
-     int val;
-    if(top_ele==-1)
-        printf("\nStack empty");
-    else
-    {
-    val=stack[top_ele];
-    printf("\nThe top_elemost element is: %d",val);
-    }
+    return 0;
 }
